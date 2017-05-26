bd09a72062b5773987e17272a08f16de44674edf
哈南的路 <wjywjwww@163.com>
2017-05-26 14:43:51 +0800

    进一步优化

---
+++ UICollectionViewDemo/EqualSpaceFlowLayoutEvolve.h
@@ -0,0 +1,13 @@
+//
+//  EqualSpaceFlowLayoutEvolve.h
+//  UICollectionViewDemo
+//
+//  Created by sks on 17/5/26.
+//  Copyright © 2017年 CHC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EqualSpaceFlowLayoutEvolve : UICollectionViewFlowLayout
+
+@end
---
+++ UICollectionViewDemo/EqualSpaceFlowLayoutEvolve.m
@@ -0,0 +1,13 @@
+//
+//  EqualSpaceFlowLayoutEvolve.m
+//  UICollectionViewDemo
+//
+//  Created by sks on 17/5/26.
+//  Copyright © 2017年 CHC. All rights reserved.
+//
+
+#import "EqualSpaceFlowLayoutEvolve.h"
+
+@implementation EqualSpaceFlowLayoutEvolve
+
+@end
